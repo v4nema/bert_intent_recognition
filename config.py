9b00d7dc9ebcdb9a8f88d7680f17bc52d278064f
v.negromaggio-at-325385785055 <v.negromaggio@reply.it>
2023-07-28 08:59:19 +0200

    Some config removed

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 eval_metric = "accuracy"
 model_name = "osiria/bert-base-italian-uncased"
 batch_size = 16
-num_epochs = 50
+num_epochs = 1
 learning_rate = 2e-5
-dataset_files = ['dataset/part1.tsv', 'dataset/part2.tsv']
-train_test_split_path = 'dataset'
+save_train_test_split = True
+
 results_path = 'results'
